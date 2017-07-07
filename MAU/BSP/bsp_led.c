a7c10144858aefbc51e5901af7b78c6be345e3a6
chengxunming <734158330@qq.com>
2017-07-07 16:25:54 +0800

    cxm
    mau can

--- MAU/BSP/bsp_led.c
+++ MAU/BSP/bsp_led.c
@@ -25,6 +25,22 @@ void LED_Init(void)
 	GPIO_SetBits(GPIOC,GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3);
 
 	/***************************************************************************
+	                      开关指示灯LED初始化
+	****************************************************************************/
+	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_14|GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+
+	GPIO_SetBits(GPIOA,GPIO_Pin_8);	//置1关闭LED
+	GPIO_SetBits(GPIOC,GPIO_Pin_14|GPIO_Pin_15);
+	
+	/***************************************************************************
 	                      状态指示灯LED初始化
 	****************************************************************************/
 	//设置状态指示灯LED推挽输出，默认置1
@@ -51,23 +67,27 @@ void LED_Init(void)
 void LED_ALL_OFF(void)
 {
 	GPIO_SetBits(GPIOA,GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7);	//置1关闭LED
-	GPIO_SetBits(GPIOB,GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_10|GPIO_Pin_11);
-	GPIO_SetBits(GPIOC,GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
+	GPIO_SetBits(GPIOC,GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3);
 	
-	GPIO_SetBits(GPIOB,GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9);	//置1关闭LED
-	GPIO_SetBits(GPIOC,GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+	GPIO_SetBits(GPIOA,GPIO_Pin_8);	//置1关闭LED
+	GPIO_SetBits(GPIOC,GPIO_Pin_14|GPIO_Pin_15);
+	
+	GPIO_SetBits(GPIOB,GPIO_Pin_6);	//置1关闭LED
+	GPIO_SetBits(GPIOC,GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12);
 	GPIO_SetBits(GPIOD,GPIO_Pin_2);
 }
 
 
 void LED_ALL_ON(void)
 {
-	GPIO_ResetBits(GPIOA,GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7);
-	GPIO_ResetBits(GPIOB,GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_10|GPIO_Pin_11);
-	GPIO_ResetBits(GPIOC,GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
+	GPIO_ResetBits(GPIOA,GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7);	//置1关闭LED
+	GPIO_ResetBits(GPIOC,GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3);
 	
-	GPIO_ResetBits(GPIOB,GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9);	
-	GPIO_ResetBits(GPIOC,GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+	GPIO_ResetBits(GPIOA,GPIO_Pin_8);	//置1关闭LED
+	GPIO_ResetBits(GPIOC,GPIO_Pin_14|GPIO_Pin_15);
+	
+	GPIO_ResetBits(GPIOB,GPIO_Pin_6);	//置1关闭LED
+	GPIO_ResetBits(GPIOC,GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12);
 	GPIO_ResetBits(GPIOD,GPIO_Pin_2);
 }
 
